606e522a0908e992ba00a889907317905b96cf0f
Kyle Alderson <kyle.alderson91@gmail.com>
2022-08-25 13:26:52 -0400

    fisrt push

---
+++ app1.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "app1" {
+  name     = "app1"
+  location = "US East1"
+}
---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+# Configure the Microsoft Azure Provider (azurerm provider) in provider blocks.
+provider "azurerm" {
+  features {}
+}
