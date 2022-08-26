359624153ddbb17cc3f6a8526d171b00d202f061
Kyle Alderson <kyle.alderson91@gmail.com>
2022-08-26 15:48:12 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "=2.46.0"
     }
   }
+  cloud {
+    organization = "kylesbx"
+    workspaces {
+      tags = ["env:#{environment}#", "app:tst-app-kyle", "platform:azure"]
+    }
+  }
 }
 # Configure the Microsoft Azure Provider (azurerm provider) in provider blocks.
 provider "azurerm" {
